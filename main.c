2ad3a3d7b6bf05ee21b7f484a7f9912d4813f88f
568j4 <144990819+568j4@users.noreply.github.com>
2023-10-15 20:07:41 +0900

    5

--- main.c
+++ main.c
@@ -2,39 +2,37 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int sumTwo(int a, int b)
-{
-	int c=a+b;
-	return c;
+int factorial(int num) {
+    if (num <= 1)
+        return 1;
+    else
+        return num * factorial(num - 1);
 }
 
-int square(int n)
+int combination(int n, int r)
 {
-	return n*n;
-}
-
-int get_max(int x, int y)
-{
-	if (x>y)
-	 return x;
-	 
-	return y; 
+	int div1, div2;
+	
+	div1=factorial(n);
+	div2=factorial(n-r)*factorial(r);
+	
+	return (div1/div2);
 }
 
 int main(int argc, char *argv[])
 {
-	int a,b;
+	int n,r;
+	int result;
+	
+	printf("input n: ");
+	scanf("%d", &n);
+	
+	printf("input r: ");
+    scanf("%d", &r);
 
+    result = combination(n, r);
+    printf("C(%d, %d) = %d\n", n, r, result);
+    
+    return 0;
 
-a=3;
-b=10;
-printf("sumTwo result: %i\n", sumTwo(a,b) );
-
-printf("square result: %i\n", square(a) );
-
-printf("get_max result: %i\n", get_max(a,b) );
-
-system("PAUSE");
-return 0;
 }
